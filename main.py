6cb2b5d837602f58d218d3aa372f6469654fe2e6
Aarsh Mehta <113206841+aarsh-mehta@users.noreply.github.com>
2023-11-12 04:47:04 -0500

    Final code

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ from watchdog.events import FileSystemEventHandler
 pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/bin/tesseract'  # Update this path
 
 braille_dict = {
-    'a': '100000', 'b': '101000', 'c': '110000', 'd': '110100',
+    'a': '100000', 'b': ' ', 'c': '110000', 'd': '110100',
     'e': '100100', 'f': '111000', 'g': '111100', 'h': '101100',
     'i': '011000', 'j': '011100', 'k': '100010', 'l': '101010',
     'm': '110010', 'n': '110110', 'o': '100110', 'p': '111010',
     'q': '111110', 'r': '101110', 's': '011010', 't': '011110',
     'u': '100011', 'v': '101011', 'w': '011101', 'x': '110011',
-    'y': '110111', 'z': '100111',
+    'y': '110111', 'z': '100111', ' ': '000000'
     # Add more mappings for numbers and punctuation if needed.
 }
 # Serial Communication Setup
@@ -24,15 +24,33 @@ arduino_serial = serial.Serial(port='/dev/tty.usbmodem1101', baudrate=9600, time
 time.sleep(2)  # Wait for connection to establish
 
 def text_to_braille(text):
-    return ''.join(braille_dict.get(char, '000000') for char in text.lower())
+    braille_string = ""
+    for char in text.lower():  # Convert the entire text to lowercase
+        braille_string += braille_dict.get(char, '000000')  # Default to '000000' for unknown characters
+    return braille_string
 
+
+# def process_image(image_path):
+#     print("Processing image:", image_path)
+#     text = pytesseract.image_to_string(Image.open(image_path))
+#     print("Extracted text:", text)
+#     braille = text_to_braille(text)
+#     print("Braille representation:", braille)
+#     arduino_serial.write(braille.encode())
 def process_image(image_path):
     print("Processing image:", image_path)
     text = pytesseract.image_to_string(Image.open(image_path))
     print("Extracted text:", text)
     braille = text_to_braille(text)
     print("Braille representation:", braille)
-    arduino_serial.write(braille.encode())
+
+    # Send each 6-bit Braille character separately
+    for i in range(0, len(braille), 6):
+        braille_char = braille[i:i+6]
+        print(braille[i:i+6])
+        arduino_serial.write(braille_char.encode())
+        time.sleep(0.2)  # Short delay between characters; adjust as needed
+
 
 class Watcher:
     def __init__(self, directory_to_watch):
