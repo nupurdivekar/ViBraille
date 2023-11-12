76b4bb1c6121d0f611dd74bd206d44bf2170b57c
Nupur <ndivekar@bu.edu>
2023-11-11 20:36:04 -0500

    added dictionary

--- main.py
+++ main.py
@@ -7,14 +7,25 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 # Tesseract executable path
-pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/bin/tesseract'
+pytesseract.pytesseract.tesseract_cmd = r'path_to_tesseract.exe'
 
 # Serial port configuration
 ser = serial.Serial('COM_PORT', 9600)  # Update COM port as needed
 
 # Braille conversion dictionary
 braille_dict = {
-    # Your alphabet to braille mapping here
+    'a': '100000', 'b': '101000', 'c': '110000', 'd': '110100',
+    'e': '100100', 'f': '111000', 'g': '111100', 'h': '101100',
+    'i': '011000', 'j': '011100', 'k': '100010', 'l': '101010',
+    'm': '110010', 'n': '110110', 'o': '100110', 'p': '111010',
+    'q': '111110', 'r': '101110', 's': '011010', 't': '011110',
+    'u': '100011', 'v': '101011', 'w': '011101', 'x': '110011',
+    'y': '110111', 'z': '100111', 
+    ' ': '000000',  # Space
+    '1': '100000', '2': '101000', '3': '110000', '4': '110100', 
+    '5': '100100', '6': '111000', '7': '111100', '8': '101100', 
+    '9': '011100', '0': '011000',
+    # Add more mappings for punctuation if needed
 }
 
 # Function to convert text to braille
